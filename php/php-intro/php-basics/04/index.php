9c08a2353a42320f32fdafc171fe0be047f94667
gzwan <gzwan2@hotmail.com>
2023-07-22 02:17:02 +0300

    add all

---
+++ php/php-intro/php-basics/04/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// PHP is Loosely typed 
+
+
+ //echo gettype('hello');
+
+//echo gettype(1);
+
+
+//echo null ;
+
+
+// Casting 
+
+//To String 
+
+// $var = 10 ;
+
+// echo gettype( (string) $var);
+
+//To Integer 
+
+//echo (integer) '5hello';
+
+echo (boolean) null ;
+
+
