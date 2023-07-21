37499ad18d3a49b6d3195549dee879a644b7ee6d
gzwan <gzwan2@hotmail.com>
2023-07-22 01:45:12 +0300

    add all

---
+++ php/php-intro/php-basics/03/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Strings
+
+$name = 'Hsoub' ;
+
+// Numbers
+
+$age = 28 ; // integer
+
+$ip = 3.14 ; // double
+
+// Booleans (true or false)
+
+$rainy = false ;
+
+// null
+
+$empty = null ;
+
+$name = null ;
+
+echo "Hello , $name ";
