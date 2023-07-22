98e3f3e087c658ebe4f254f86565309de580c958
gzwan <gzwan2@hotmail.com>
2023-07-22 07:14:18 +0300

    add all

---
+++ php/php-intro/conditional-statements/04/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// isset 
+
+// $a = 1 ;
+
+const MAX = 100 ;
+
+//if (isset($a)){
+//  echo "defined" ;
+//}else{
+//  echo "undefined" ;
+//}
+
+
+//echo isset($a) ? "defined" : "undefined" ;
+
+
+echo defined("MAX") ? "defined" : "undefined" ;
