12570973f64ee3662a6d7011db9e533787a2b1df
gzwan <gzwan2@hotmail.com>
2023-07-22 03:43:14 +0300

    add all

---
+++ php/php-intro/operators/01/index.php
@@ -0,0 +1,48 @@
+<?php
+
+$a = 6 ;
+
+$b = 3 ;
+
+echo $a + $b + 5 ; 
+
+echo '<br />' ;
+
+echo $a - $b ;
+
+echo '<br />' ;
+
+
+echo $a * $b ;
+
+echo '<br />' ;
+
+
+echo $a / $b ;
+
+echo '<br />' ;
+
+
+echo $a ** $b ;
+
+echo '<br />' ;
+
+
+echo $a % $b ;
+
+echo '<br />' ;
+
+
+echo $a++ ;
+
+echo '<br />' ;
+
+echo --$a ;
+
+echo '<br />' ;
+
+
+
+
+
+
