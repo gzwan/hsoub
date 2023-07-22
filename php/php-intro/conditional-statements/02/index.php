5631aae84d6aa08d6626906e988f123b5950e23c
gzwan <gzwan2@hotmail.com>
2023-07-22 06:54:28 +0300

    add all

---
+++ php/php-intro/conditional-statements/02/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$i = 5 ;
+
+switch($i){
+  
+  case 0:
+   echo " i is equle to 0";
+   break;
+
+  case 1:
+    echo " i is equle to 1";
+    break;
+
+  case 2:
+    echo " i is equle to 2";
+  break;
+
+default:
+  echo " i is not equle to 0,1 or 2";
+
+}
