fe88b60e6210e31c23ec74c098810eca3b0c51f1
gzwan <gzwan2@hotmail.com>
2023-07-26 09:57:43 +0300

    add all

---
+++ php/php-intro/functions/07/index.php
@@ -0,0 +1,20 @@
+<?php
+
+//Strings
+
+// $str = 'abcdef';
+// echo strlen($str); // 6
+// $str = ' ab cd ';
+// echo strlen($str); // 7
+
+// $str = "Mary Had A Little Lamb and She LOVED It So";
+// $str = strtolower($str);
+// echo $str; // mary had a little lamb and she loved it so
+
+$str = 'abcdef';
+
+// echo  strpos($str, 'ab');
+
+// echo str_replace('cd','23',$str);
+
+echo substr($str,-2,1);
